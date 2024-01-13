7269607da2e82b04fe1c0ba5f48eda6783eaa33c
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-01-12 23:36:35 -0500

    Spinner v10 polished

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if prompt := st.chat_input("How can I help you?"):
         st.write(prompt)
 
     with st.chat_message('assistant'):
-        st.write("Thinking .")
+        st.write("Thinking ......")
 
     message_data = {
         "thread_id": st.session_state.thread.id,
